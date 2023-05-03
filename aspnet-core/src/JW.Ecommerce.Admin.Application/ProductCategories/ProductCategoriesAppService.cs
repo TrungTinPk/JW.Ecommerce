278034547275a2c22acccad0a8625b747552bc54
tintran1 <tinttran@outlook.com>
2023-05-03 20:32:39 +0700

    Create CRUD for Product Category

---
+++ aspnet-core/src/JW.Ecommerce.Admin.Application/ProductCategories/ProductCategoriesAppService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace JW.Ecommerce.ProductCategories
+{
+    public class ProductCategoriesAppService: 
+        CrudAppService<ProductCategory,ProductCategoryDto,Guid,PagedResultRequestDto,CreateUpdateProductRequestDto,CreateUpdateProductRequestDto>,
+        IProductCategoriesAppService
+    {
+        public ProductCategoriesAppService(IRepository<ProductCategory,Guid> repository): base(repository)
+        {
+            
+        }
+    }
+}
